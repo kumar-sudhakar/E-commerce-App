0b07472a0b6c45873e1bcc9fd508321c77a3e8d3
Sudhakar <kumarsudhakar213@gmail.com>
2026-09-12 14:21:58 +0530

    feat: add OurPolicy component for store policies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
 
 const home = () => {
   return (
     <>
     <Hero />
       <LatestCollection />
-      <BestSeller/>
+      <BestSeller />
+      <OurPolicy/>
       </>
 
   )
