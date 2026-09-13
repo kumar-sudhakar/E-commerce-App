0f8640b24c4ec1aaeeaa630666123645e5af84bf
Sudhakar <kumarsudhakar213@gmail.com>
2026-09-13 11:30:23 +0530

    feat: add SearchBar component UI

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,17 +1,18 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/assets";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-    
+
     const currency = '$';
     const delivery_fee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
 
     const value = {
-        currency,
-        delivery_fee,
-        products
+        products, currency, delivery_fee,
+        search, setSearch, showSearch, setShowSearch
     }
 
     return (
@@ -19,6 +20,7 @@ const ShopContextProvider = (props) => {
             {props.children}
         </ShopContext.Provider>
     )
+
 }
 
 export default ShopContextProvider;
